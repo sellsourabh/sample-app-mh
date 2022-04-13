7a8d29b7dfa9b51a24c33caf558d6c050f302e1c
Sourabh <sourabh.kalel@sell.do>
2022-04-13 14:13:54 +0530

    add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,9 +14,9 @@ module SessionsHelper
     user == current_user
   end
   
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  # end
 
   def logged_in?
     !current_user.nil?
@@ -27,7 +27,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif(user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
